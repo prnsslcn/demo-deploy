44d6f0f121718713d5dfeb76bebfe1193b43d85f
prnsslcn <prnsslcn@gmail.com>
2025-01-17 11:46:40 +0900

    Feat: GitAction 빌드 작업 추가, EC2 업로드

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -11,6 +11,7 @@ class DemoDeployApplicationTests {
         // 단위 테스트
         // MVC 테스트 -> 목업통해서 가상 처리 가능
         // RestAPI 테스트 가능
+        // 메시지가 보이지는 않지만, 차후 코드로 확인
         System.out.println("단위 테스트 호출 echo");
     }
 
