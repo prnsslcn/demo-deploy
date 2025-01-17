90f36f3af028b43b1dcfc2385af4c6f484f9371b
prnsslcn <prnsslcn@gmail.com>
2025-01-17 14:44:04 +0900

    Feat: 단위 테스트 적용 체크 (오류 발생)

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -8,8 +8,17 @@ class DemoDeployApplicationTests {
 
     @Test
     void contextLoads() {
+        // 단위 테스트
+        // 에러 강제 발생 시킴 -> 중단 여부 체크
+        // 테스트 실패되면 반영 X
         int a = 1/0;
         throw new RuntimeException("오류발생");
+        // MVC 테스트 -> 목업통해서 가상 처리 가능
+        // RestAPI 테스트 가능
+        // 메시지가 보이지는 않지만, 차후 코드로 확인
+        // System.out.println("단위 테스트 호출 echo");
     }
+
+
 
 }
